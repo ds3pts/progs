1d64a983adddc43a7c08b7bfc7787c48fea4ad99
deer42 <d.rohlmann@outlook.de>
2016-05-13 11:40:38 +0200

    Add files via upload

---
+++ qfolge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Q_Folgen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Q[i] = Q[i-Q[i-1]] + Q[i-Q[i-2]]
+            int[] Q = new int[100];
+
+            Q[0] = Q[1] = 1;
+            Console.WriteLine("1:\t" + Q[0]);
+            Console.WriteLine("2:\t" + Q[1]);
+
+            for (int i = 2; i < 100; i++)
+            {
+                Q[i] = Q[i - Q[i - 1]] + Q[i - Q[i - 2]];
+                Console.WriteLine(i+1 + ":\t" + Q[i]);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
